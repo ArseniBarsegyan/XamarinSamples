9d6c3866af142b23352a6a7edfdabe85d14743f4
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2018-06-03 10:04:39 +0300

    Added EntryWithBorder

---
+++ Templates/Templates/Templates.Android/Renderers/EntryWithBorderRenderer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Android.Content;
+using Android.Graphics;
+using Android.Graphics.Drawables;
+using Android.Util;
+using Templates.Controls;
+using Templates.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(EntryWithBorder), typeof(EntryWithBorderRenderer))]
+namespace Templates.Droid.Renderers
+{
+    /// <summary>
+    /// Renderer for <see cref="EntryWithBorder"/>
+    /// </summary>
+    public class EntryWithBorderRenderer : EntryRenderer
+    {
+        private ShapeDrawable _borderShape;
+
+        public EntryWithBorderRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null && Element != null && Control != null)
+            {
+                var view = Element as EntryWithBorder;
+
+                _borderShape = new ShapeDrawable(new Android.Graphics.Drawables.Shapes.RoundRectShape(new[]
+                {
+                    (float)0,
+                    (float)0,
+                    (float)0,
+                    (float)0,
+                    (float)0,
+                    (float)0,
+                    (float)0,
+                    (float)0
+                }, null, null));
+
+                var paint = new Paint(PaintFlags.AntiAlias)
+                {
+                    Color = view.BorderColor.ToAndroid(),
+                    StrokeWidth = 2.5f,
+                    StrokeMiter = 10f
+                };
+
+                _borderShape.Paint.Set(paint);
+                _borderShape.Paint.SetStyle(Paint.Style.Stroke);
+                Background = _borderShape;
+
+                var gradientBackground = new GradientDrawable();
+                gradientBackground.SetShape(ShapeType.Rectangle);
+                gradientBackground.SetColor(view.BackgroundColor.ToAndroid());
+
+                Control.SetBackground(gradientBackground);
+
+                Control.SetPadding((int)DpToPixels(Context, Convert.ToSingle(12)), Control.PaddingTop,
+                    (int)DpToPixels(Context, Convert.ToSingle(12)), Control.PaddingBottom);
+            }
+        }
+
+        public static float DpToPixels(Context context, float valueInDp)
+        {
+            DisplayMetrics metrics = context.Resources.DisplayMetrics;
+            return TypedValue.ApplyDimension(ComplexUnitType.Dip, valueInDp, metrics);
+        }
+    }
+}
---
+++ Templates/Templates/Templates.iOS/Renderers/EntryWithBorderRenderer.cs
@@ -0,0 +1,29 @@
+﻿using Templates.Controls;
+using Templates.iOS.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(EntryWithBorder), typeof(EntryWithBorderRenderer))]
+namespace Templates.iOS.Renderers
+{
+    /// <summary>
+    /// Renderer for <see cref="EntryWithBorder"/>
+    /// </summary>
+    public class EntryWithBorderRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                var view = Element as EntryWithBorder;
+                view.HeightRequest = 40;
+
+                Control.Layer.BorderColor = view.BorderColor.ToCGColor();
+                Control.Layer.BorderWidth = 1;
+                Control.ClipsToBounds = true;
+            }
+        }
+    }
+}
