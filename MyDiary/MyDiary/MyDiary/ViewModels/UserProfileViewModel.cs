d255ca05b5065a519f17031d21bdcf753448fba7
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2018-06-10 21:45:35 +0300

    Added UserProfilePage, UserProfileViewModel (empty)

---
+++ MyDiary/MyDiary/MyDiary/ViewModels/UserProfileViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MyDiary.ViewModels
+{
+    public class UserProfileViewModel : BaseViewModel
+    {
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
