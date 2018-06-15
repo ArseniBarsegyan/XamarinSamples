0967fce11ab47467b7820af971244280c525ed55
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2018-06-16 01:04:07 +0300

    MyDiary removed from solution.

--- MyDiary/MyDiary/MyDiary/ViewModels/BaseViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel;
-
-namespace MyDiary.ViewModels
-{
-    public class BaseViewModel : INotifyPropertyChanged
-    {
-        public event PropertyChangedEventHandler PropertyChanged;
-    }
-}
--- MyDiary/MyDiary/MyDiary/ViewModels/UserProfileViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MyDiary.ViewModels
-{
-    public class UserProfileViewModel : BaseViewModel
-    {
-        public string Name { get; set; }
-        public string ImageUrl { get; set; }
-    }
-}
