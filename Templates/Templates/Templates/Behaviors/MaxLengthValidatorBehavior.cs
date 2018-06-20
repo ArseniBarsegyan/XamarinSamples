9d2904332430d4c9aacf83d0f9ef9299350b0e5e
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2018-06-20 23:24:09 +0300

    Added Max and Min length validator behaviors.

---
+++ Templates/Templates/Templates/Behaviors/MaxLengthValidatorBehavior.cs
@@ -0,0 +1,64 @@
+﻿using Xamarin.Forms;
+
+namespace Templates.Behaviors
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Checks maximum length of the Entry's text.
+    /// </summary>
+    public class MaxLengthValidatorBehavior : Behavior<Entry>
+    {
+        private static readonly BindablePropertyKey IsValidPropertyKey = BindableProperty.CreateReadOnly(nameof(IsValid), typeof(bool), typeof(MaxLengthValidatorBehavior), false);
+        private static readonly BindableProperty ValidNumberTextColorProperty = BindableProperty.Create(nameof(ValidNumberTextColor), typeof(Color), typeof(MaxLengthValidatorBehavior), Color.Black);
+        private static readonly BindableProperty InvalidNumberTextColorProperty = BindableProperty.Create(nameof(InvalidNumberTextColor), typeof(Color), typeof(MaxLengthValidatorBehavior), Color.Red);
+        public static readonly BindableProperty IsValidProperty = IsValidPropertyKey.BindableProperty;
+        public static readonly BindableProperty MaxLengthProperty = BindableProperty.Create(nameof(MaxLength), typeof(int), typeof(MaxLengthValidatorBehavior), 1);
+
+        public bool IsValid
+        {
+            get => (bool)GetValue(IsValidProperty);
+            private set => SetValue(IsValidPropertyKey, value);
+        }
+
+        public Color ValidNumberTextColor
+        {
+            get => (Color)GetValue(ValidNumberTextColorProperty);
+            set => SetValue(ValidNumberTextColorProperty, value);
+        }
+
+        public Color InvalidNumberTextColor
+        {
+            get => (Color)GetValue(InvalidNumberTextColorProperty);
+            set => SetValue(InvalidNumberTextColorProperty, value);
+        }
+
+        public int MaxLength
+        {
+            get => (int)GetValue(MaxLengthProperty);
+            set => SetValue(MaxLengthProperty, value);
+        }
+
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            bindable.TextChanged += Bindable_TextChanged;
+        }
+
+        private void Bindable_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (e.NewTextValue.Length > 0 && e.NewTextValue.Length < MaxLength)
+            {
+                IsValid = true;
+            }
+            else
+            {
+                IsValid = false;
+            }
+            ((Entry)sender).TextColor = IsValid ? ValidNumberTextColor : InvalidNumberTextColor;
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            bindable.TextChanged -= Bindable_TextChanged;
+        }
+    }
+}
---
+++ Templates/Templates/Templates/Behaviors/MinLengthValidatorBehavior.cs
@@ -0,0 +1,64 @@
+﻿using Xamarin.Forms;
+
+namespace Templates.Behaviors
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Checks minimum length of the Entry's text.
+    /// </summary>
+    public class MinLengthValidatorBehavior : Behavior<Entry>
+    {
+        private static readonly BindablePropertyKey IsValidPropertyKey = BindableProperty.CreateReadOnly(nameof(IsValid), typeof(bool), typeof(MinLengthValidatorBehavior), false);
+        private static readonly BindableProperty ValidNumberTextColorProperty = BindableProperty.Create(nameof(ValidNumberTextColor), typeof(Color), typeof(MinLengthValidatorBehavior), Color.Black);
+        private static readonly BindableProperty InvalidNumberTextColorProperty = BindableProperty.Create(nameof(InvalidNumberTextColor), typeof(Color), typeof(MinLengthValidatorBehavior), Color.Red);
+        public static readonly BindableProperty MinLengthProperty = BindableProperty.Create(nameof(MinLength), typeof(int), typeof(MinLengthValidatorBehavior), 1);
+        public static readonly BindableProperty IsValidProperty = IsValidPropertyKey.BindableProperty;
+
+        public bool IsValid
+        {
+            get => (bool)GetValue(IsValidProperty);
+            private set => SetValue(IsValidPropertyKey, value);
+        }
+
+        public Color ValidNumberTextColor
+        {
+            get => (Color)GetValue(ValidNumberTextColorProperty);
+            set => SetValue(ValidNumberTextColorProperty, value);
+        }
+
+        public Color InvalidNumberTextColor
+        {
+            get => (Color)GetValue(InvalidNumberTextColorProperty);
+            set => SetValue(InvalidNumberTextColorProperty, value);
+        }
+
+        public int MinLength
+        {
+            get => (int)GetValue(MinLengthProperty);
+            set => SetValue(MinLengthProperty, value);
+        }
+
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            bindable.TextChanged += Bindable_TextChanged;
+        }
+
+        private void Bindable_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (e.NewTextValue.Length > 0 && e.NewTextValue.Length >= MinLength)
+            {
+                IsValid = true;
+            }
+            else
+            {
+                IsValid = false;
+            }
+            ((Entry)sender).TextColor = IsValid ? ValidNumberTextColor : InvalidNumberTextColor;
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            bindable.TextChanged -= Bindable_TextChanged;
+        }
+    }
+}
